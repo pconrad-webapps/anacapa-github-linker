e9947cf89ff9fa05dd14b9f18045fb791c12632d
Phill Conrad <phillip.conrad@appfolio.com>
2019-07-12 11:35:15 -0700

    pc/team - use af_json_api pagination

--- app/api/api/v1/users.rb
+++ app/api/api/v1/users.rb
@@ -3,18 +3,9 @@ module Api
     class Users < Base
       resources :users do
         desc 'List Users', resource: Api::Resources::V1::User
-        params do
-          optional :page, type: Integer
-          optional :per_page, type: Integer
-        end
+        paginate
         get do
-          if params[:per_page] && params[:page]
-            present User.limit(params[:per_page]).offset(params[:per_page]*(params[:page]-1)), with: Api::V1::Entities::User
-          else
-            present User.all, with: Api::V1::Entities::User
-          end
-
-          #present User.page(params[:page]).per(params[:per_page]), with: Api::V1::Entities::User
+          present User.all
         end
       end
     end
