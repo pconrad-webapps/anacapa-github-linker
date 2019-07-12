cb1169caf0c2ccf6d0c0b4b31b6d3c8386cf9f47
Jacqui Mai <jacqueline.mai@appfolio.com>
2019-07-11 17:52:26 -0700

    jm/team - Added pagination for users endpoint for API

--- app/api/api/v1/users.rb
+++ app/api/api/v1/users.rb
@@ -3,9 +3,18 @@ module Api
     class Users < Grape::API
       resources :users do
         desc 'List Users'#, resource: Api::Resources::V1::User
-        # paginate
+        params do
+          optional :page, type: Integer
+          optional :per_page, type: Integer
+        end
         get do
-          present User.all, with: Api::V1::Entities::User
+          if params[:per_page] && params[:page]
+            present User.limit(params[:per_page]).offset(params[:per_page]*(params[:page]-1)), with: Api::V1::Entities::User
+          else
+            present User.all, with: Api::V1::Entities::User
+          end
+
+          #present User.page(params[:page]).per(params[:per_page]), with: Api::V1::Entities::User
         end
       end
     end
